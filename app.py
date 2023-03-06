0d668a5886f67030554603b531cf451624d24bef
JHawi13 <Joherwi@gmail.com>
2023-03-06 10:19:56 +0300

    "6TH March commit"

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app=Flask(__name__)
 @app.route("/")
 def homepage():
     return render_template("Portfolio_homepage/Portfolio.html")
+@app.route('/contact/')
+def contact_page():
+    return render_template("Portfolio_homepage/Forms/contact_form.html")
